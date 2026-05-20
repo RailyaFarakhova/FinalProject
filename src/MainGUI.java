37311bf264eb8bdeae2be629b2751a029a509a03
lizkesh <elizabeth.keshishyan@ht-la.org>
2026-05-20 10:24:48 -0700

    Start of GUI

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,3 +1,52 @@
-public class MainGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+public class MainGUI implements ActionListener {
+    JFrame frame;
+    JPanel panel;
+    JButton librarianButton;
+    JButton userButton;
+    JLabel selector;
+
+    String librarianCode;
+
+    public MainGUI() {
+        frame = new JFrame ("Library");
+        librarianButton = new JButton("I'm a Librarian");
+        librarianButton.addActionListener(e-> {
+            librarianCode = JOptionPane.showInputDialog("Please enter your code:");
+
+            //Logic
+        });
+
+        userButton = new JButton("I'm a Guest");
+        userButton.addActionListener(e-> {
+
+            //Logic
+        });
+        panel = new JPanel();
+        selector = new JLabel("Login");
+
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 100, 50, 100));
+        panel.setLayout(new GridLayout(2,3));
+
+        panel.setPreferredSize(new Dimension(500, 200));
+
+        panel.add(selector);
+        panel.add(userButton);
+        panel.add(librarianButton);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
