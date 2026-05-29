1a95338b171748d65232f9fe511867be116bc032
RailyaFarakhova <railya.farakhova@ht-la.org>
2026-05-28 19:45:42 -0700

    Merged file

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         // This is used to test the program. :)
         BookLogic bookLogic = new BookLogic();
 
-        bookLogic.importBooksCSV("book.csv");
+        bookLogic.importBooks("CSV", "book.csv");
         System.out.println(bookLogic.getTitles());
 
-//        bookLogic.removeBook("Title", "The lorax");
-//        System.out.println(bookLogic.getTitles());
-//
-//        bookLogic.updateBook("over the garden wall", "title", "Dr Cucumber");
-//        System.out.println(bookLogic.getTitles());
-//
-//        bookLogic.addBook("Lord of the Rings", "Squirtle", 2020);
-//        System.out.println(bookLogic.getTitles());
+        bookLogic.removeBook("Title", "The lorax");
+        System.out.println(bookLogic.getTitles());
 
-        bookLogic.getBook("Title", "The lorax").checkOut("mochi", "mochi@gmail.com");
-        System.out.println(bookLogic.getBook("Title", "The lorax").getCheckoutDate());
-        System.out.println(bookLogic.getBook("Title", "The lorax").getDueDate());
+        bookLogic.updateBook("over the garden wall", "title", "Dr Cucumber");
+        System.out.println(bookLogic.getTitles());
+
+        bookLogic.addBook("Lord of the Rings", "Squirtle", 2020);
+        System.out.println(bookLogic.getTitles());
+
+        bookLogic.getBook("Title", "Lord of the Rings").checkOut("mochi", "mochi@gmail.com");
+        System.out.println(bookLogic.getBook("Title", "Lord of the Rings").getCheckoutDate());
+        System.out.println(bookLogic.getBook("Title", "Lord of the Rings").getDueDate());
 
         new MainGUI();
     }
